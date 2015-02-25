c0f12a8775e3422e81952c859e515fecbe2bb9a1
davidghi <david.ghiurau@gmail.com>
2015-02-25 17:15:45 +0200

    improve touch swipe support on touch-enabled desktop browsers

--- src/js/jquery.gridAccordion.touchSwipe.js
+++ src/js/jquery.gridAccordion.touchSwipe.js
@@ -11,8 +11,6 @@
 
 	var TouchSwipe = {
 
-		isTouchSupport: false,
-
 		touchStartPoint: {x: 0, y: 0},
 
 		touchEndPoint: {x: 0, y: 0},
@@ -23,6 +21,8 @@
 
 		isTouchMoving: false,
 
+		touchSwipeEvents: { startEvent: '', moveEvent: '', endEvent: '' },
+
 		initTouchSwipe: function() {
 			var that = this;
 
@@ -30,24 +30,13 @@
 			if (this.settings.touchSwipe === false)
 				return;
 
-			// check if there is touch support
-			this.isTouchSupport = 'ontouchstart' in window;
-
-			// listen to touch events or, if touch support doesn't exist, listen to mouse events
-			var startEvent = this.isTouchSupport ? 'touchstart' : 'mousedown';
-			this.$panelsContainer.on(startEvent + '.' + NS, $.proxy(this._onTouchStart, this));
+			this.touchSwipeEvents.startEvent = 'touchstart' + '.' + NS + ' mousedown' + '.' + NS;
+			this.touchSwipeEvents.moveEvent = 'touchmove' + '.' + NS + ' mousemove' + '.' + NS;
+			this.touchSwipeEvents.endEvent = 'touchend' + '.' + this.uniqueId + '.' + NS + ' mouseup' + '.' + this.uniqueId + '.' + NS;
 			
-			this.on('update.TouchSwipe.' + NS, function() {
-				// remove mouse events on panels
-				if (that.isTouchSupport)
-					$.each(that.panels, function(index, element) {
-						var panel = element;
-						panel.off('panelMouseOver.' + NS);
-						panel.off('panelMouseOut.' + NS);
-						panel.off('panelClick.' + NS);
-						panel.off('panelMouseDown.' + NS);
-					});
+			this.$panelsContainer.on(this.touchSwipeEvents.startEvent, $.proxy(this._onTouchStart, this));
 
+			this.on('update.TouchSwipe.' + NS, function() {
 				// add or remove grabbing icon
 				if (that.getTotalPages() > 1)
 					that.$panelsContainer.addClass('ga-grab');
@@ -57,18 +46,16 @@
 		},
 
 		_onTouchStart: function(event) {
+			var that = this,
+				eventObject =  typeof event.originalEvent.touches !== 'undefined' ? event.originalEvent.touches[0] : event.originalEvent;
+
 			// disable dragging if the element is set to allow selections
-			if ($(event.target).closest('.ga-selectable').length >= 1 || (this.isTouchSupport === false && this.getTotalPages() === 1))
+			if ($(event.target).closest('.ga-selectable').length >= 1 || (typeof event.originalEvent.touches === 'undefined' && this.getTotalPages() === 1))
 				return;
 
 			// prevent default behavior only for mouse events
-			if (this.isTouchSupport === false)
+			if (typeof event.originalEvent.touches === 'undefined')
 				event.preventDefault();
-
-			var that = this,
-				eventObject = this.isTouchSupport ? event.originalEvent.touches[0] : event.originalEvent,
-				moveEvent = this.isTouchSupport ? 'touchmove' : 'mousemove',
-				endEvent = this.isTouchSupport ? 'touchend' : 'mouseup';
 
 			// get the initial position of the mouse pointer and the initial position of the panels' container
 			this.touchStartPoint.x = eventObject.pageX || eventObject.clientX;
@@ -79,8 +66,8 @@
 			this.touchDistance.x = this.touchDistance.y = 0;
 
 			// listen for move and end events
-			this.$panelsContainer.on(moveEvent + '.' + NS, $.proxy(this._onTouchMove, this));
-			$(document).on(endEvent + '.' + this.uniqueId + '.' + NS, $.proxy(this._onTouchEnd, this));
+			this.$panelsContainer.on(this.touchSwipeEvents.moveEvent, $.proxy(this._onTouchMove, this));
+			$(document).on(this.touchSwipeEvents.endEvent, $.proxy(this._onTouchEnd, this));
 
 			// swap grabbing icons
 			this.$panelsContainer.removeClass('ga-grab').addClass('ga-grabbing');
@@ -94,7 +81,7 @@
 		},
 
 		_onTouchMove: function(event) {
-			var eventObject = this.isTouchSupport ? event.originalEvent.touches[0] : event.originalEvent;
+			var eventObject = typeof event.originalEvent.touches !== 'undefined' ? event.originalEvent.touches[0] : event.originalEvent;
 
 			// indicate that the move event is being fired
 			this.isTouchMoving = true;
@@ -129,21 +116,20 @@
 
 		_onTouchEnd: function(event) {
 			// remove the move and end listeners
-			var that = this,
-				moveEvent = this.isTouchSupport ? 'touchmove' : 'mousemove',
-				endEvent = this.isTouchSupport ? 'touchend' : 'mouseup';
+			var that = this;
 
-			this.$panelsContainer.off(moveEvent + '.' + NS);
-			$(document).off(endEvent + '.' + this.uniqueId + '.' + NS);
+			this.$panelsContainer.off(this.touchSwipeEvents.moveEvent);
+			$(document).off(this.touchSwipeEvents.endEvent);
 
 			// swap grabbing icons
 			this.$panelsContainer.removeClass('ga-grabbing').addClass('ga-grab');
 
 			// check if there is intention for a tap
-			if (this.isTouchSupport === true && (this.isTouchMoving === false || this.isTouchMoving === true && Math.abs(this.touchDistance.x) < 10 && Math.abs(this.touchDistance.y) < 10)) {
+			if (typeof event.originalEvent.touches !== 'undefined' && (this.isTouchMoving === false || this.isTouchMoving === true && Math.abs(this.touchDistance.x) < 10 && Math.abs(this.touchDistance.y) < 10)) {
 				var index = $(event.target).parents('.ga-panel').index();
 
 				if (index !== this.currentIndex && index !== -1) {
+					event.preventDefault();
 					this.openPanel(index);
 				} else {
 					// re-enable click events on links
@@ -160,6 +146,10 @@
 				this.$accordion.removeClass('ga-swiping');
 				return;
 			}
+
+			$(event.target).parents('.ga-panel').one('click', function(event) {
+				event.preventDefault();
+			});
 
 			this.isTouchMoving = false;
 
@@ -211,13 +201,9 @@
 		},
 
 		destroyTouchSwipe: function() {
-			var startEvent = this.isTouchSupport ? 'touchstart' : 'mousedown',
-				endEvent = this.isTouchSupport ? 'touchend' : 'mouseup',
-				moveEvent = this.isTouchSupport ? 'touchmove' : 'mousemove';
-
-			this.$panelsContainer.off(startEvent + '.' + NS);
-			$(document).off(endEvent + '.' + this.uniqueId + '.' + NS);
-			this.$panelsContainer.off(moveEvent + '.' + NS);
+			this.$panelsContainer.off(this.touchSwipeEvents.startEvent);
+			$(document).off(this.touchSwipeEvents.endEvent);
+			this.$panelsContainer.off(this.touchSwipeEvents.moveEvent);
 			this.off('update.TouchSwipe.' + NS);
 		},
 
